74775c8572fa95ed0f709585a2e0b95a815036de
odracir20 <odracir20@gmail.com>
2025-08-04 17:57:15 -0600

    Estilos anidados

--- js/menu.js
+++ js/menu.js
@@ -2,10 +2,17 @@ const nav = document.querySelector("#navegador");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
-abrir.addEventListener("click", () => {
-    nav.classList.add ("visible");
-})
 
-cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
+function abrirMenu() {
+    nav.classList.toggle("visible");
+}
+
+abrir.addEventListener("click", abrirMenu);
+cerrar.addEventListener("click", abrirMenu);
+
+const cerrarMenuLinks = document.querySelectorAll("#navegador a[href^='#']");
+cerrarMenuLinks.forEach(cerrarMenuLinks => {
+    cerrarMenuLinks.addEventListener('click', function () {
+        nav.classList.remove("visible");
+    })
 })
